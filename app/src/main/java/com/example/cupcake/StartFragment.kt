8ebc582be8726e7ca9383944122ff4e4b1f369dc
agesa3 <collinsagesa2000@gmail.com>
2021-11-13 14:48:38 +0300

    more updates

--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -21,13 +21,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentStartBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
  * This is the first screen of the Cupcake app. The user can choose how many cupcakes to order.
  */
 class StartFragment : Fragment() {
+    private val sharedViewModel:OrderViewModel by activityViewModels()
 
     // Binding object instance corresponding to the fragment_start.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
@@ -58,6 +61,10 @@ class StartFragment : Fragment() {
      * Start an order with the desired quantity of cupcakes and navigate to the next screen.
      */
     fun orderCupcake(quantity: Int) {
+        sharedViewModel.setQuantity(quantity)
+        if(sharedViewModel.hasNoFlavorSet()){
+            sharedViewModel.setFlavor(getString(R.string.vanilla))
+        }
         findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
     }
 
--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -27,4 +27,8 @@ class OrderViewModel : ViewModel() {
     fun setDate(pickupDate:String){
         _date.value=pickupDate
     }
+
+    fun hasNoFlavorSet():Boolean{
+        return _flavor.value.isNullOrEmpty()
+    }
 }
